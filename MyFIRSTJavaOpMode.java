1fba9249c154ac63d9096d8800f2367ca8b906fb
Ian Belanger <105866068+IanBelanger1@users.noreply.github.com>
2023-12-20 17:37:55 -0500

    Update MyFIRSTJavaOpMode.java

--- MyFIRSTJavaOpMode.java
+++ MyFIRSTJavaOpMode.java
@@ -9,47 +9,64 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name="Template: CHEESE OpMode", group="Linear Opmode")
+@TeleOp(name="Template: VELVEETA OpMode", group="Linear Opmode")
 public class MyFIRSTJavaOpMode extends LinearOpMode {
-        private Gyroscope imu;
-        private DcMotor[] motorTest;
-        private DigitalChannel digitalTouch;
-        private DistanceSensor sensorColorRange;
-        private Servo servoTest;
+    private Gyroscope imu;
+    private DcMotor[] motorTest;
+    private DigitalChannel digitalTouch;
+    private DistanceSensor sensorColorRange;
+    private Servo servoTest;
 
 
-        @Override
-        public void runOpMode() {
-            //imu = hardwareMap.get(Gyroscope.class, "imu");
-            motorTest = new DcMotor[4];
-            for (int i = 0; i < 4; i++) {
-                motorTest[i] = hardwareMap.get(DcMotor.class, "motor" + i);
-            }
-            //digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-            //sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-            //servoTest = hardwareMap.get(Servo.class, "servoTest");
+    @Override
+    public void runOpMode() {
+        //imu = hardwareMap.get(Gyroscope.class, "imu");
+        motorTest = new DcMotor[4];
+        for (int i = 0; i < 4; i++) {
+            motorTest[i] = hardwareMap.get(DcMotor.class, "motor" + i);
+        }
+        //digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        //sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        //servoTest = hardwareMap.get(Servo.class, "servoTest");
 
-            telemetry.addData("Status", "Initialized");
-            telemetry.update();
-            // Wait for the game to start (driver presses PLAY)
-            waitForStart();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
 
-            // run until the end of the match (driver presses STOP)
-            while (opModeIsActive()) {
-                if (this.gamepad1.x) {
-                    telemetry.addData("Status", "X");
-                } else if (this.gamepad1.y) {
-                    telemetry.addData("Status", "Y");
+        // run until the end of the match (driver presses STOP)
+        int modifier = 0;
+        boolean xPressed = false;
+        boolean yPressed = false;
+        while (opModeIsActive()) {
+            if (this.gamepad1.x) {
+                if (!xPressed && (modifier < 10)) {
+                    modifier += 1;
                 }
-                double power0 = this.gamepad1.left_stick_x;
-                motorTest[0].setPower(power0);
-                double power1 = this.gamepad1.left_stick_y;
-                motorTest[1].setPower(power1);
-                double power2 = this.gamepad1.right_stick_x;
-                motorTest[2].setPower(power2);
-                double power3 = this.gamepad1.right_stick_y;
-                motorTest[3].setPower(power3);
-                telemetry.update();
+                xPressed = true;
+            } else {
+                xPressed = false;
             }
+            if (this.gamepad1.y) {
+                //telemetry.addData("Status", "Y");
+                if (!yPressed && modifier > -10) {
+                    modifier -= 1;
+                }
+                yPressed = true;
+            } else {
+                yPressed = false;
+            }
+            telemetry.addData("Status", "" + modifier);
+            double modifier1 = (double) modifier/10;
+            double power0 = this.gamepad1.left_stick_x;
+            motorTest[0].setPower(power0 * modifier1);
+            double power1 = this.gamepad1.left_stick_y;
+            motorTest[1].setPower(power1 * modifier1);
+            double power2 = this.gamepad1.right_stick_x;
+            motorTest[2].setPower(power2 * modifier1);
+            double power3 = this.gamepad1.right_stick_y;
+            motorTest[3].setPower(power3 * modifier1);
+            telemetry.update();
         }
     }
+}
