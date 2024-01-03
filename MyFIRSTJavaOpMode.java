57ffdf05a876e266da5cbca6b71f919779dcc240
DelphinusArcana <146129635+DelphinusArcana@users.noreply.github.com>
2024-01-03 13:56:55 -0500

    Update MyFIRSTJavaOpMode.java
    Put the initializing in runOpMode() so no exceptions

--- MyFIRSTJavaOpMode.java
+++ MyFIRSTJavaOpMode.java
@@ -9,17 +9,30 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name="Template: 2024MovementOpmode1", group="Linear Opmode")
-public class MyFIRSTJavaOpMode extends LinearOpMode {
-    private DcMotor lfd = hardwareMap.get(DcMotor.class, "leftFrontDrive");
-    private DcMotor lrd = hardwareMap.get(DcMotor.class, "leftRearDrive");
-    private DcMotor rfd = hardwareMap.get(DcMotor.class, "rightFrontDrive");
-    private DcMotor rrd = hardwareMap.get(DcMotor.class, "rightRearDrive");
+@TeleOp(name="Template: 2024TranslateLiftOpmode1", group="Linear Opmode")
+public class Zac_2023_2 extends LinearOpMode {
+    private DcMotor lfd;
+    private DcMotor lrd;
+    private DcMotor rfd;
+    private DcMotor rrd;
+    private DcMotor lfl;
+    private DcMotor lrl;
+    private DcMotor rfl;
+    private DcMotor rrl;
     private int lMod = -1; // changes CW to CCW for left motors
     private int rMod = 1; // changes CW to CCW for right motors
     @Override
     public void runOpMode() {
-
+        lfd = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        lrd = hardwareMap.get(DcMotor.class, "leftRearDrive");
+        rfd = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        rrd = hardwareMap.get(DcMotor.class, "rightRearDrive");
+        /*
+        lfl = hardwareMap.get(DcMotor.class, "leftFrontLift");
+        lrl = hardwareMap.get(DcMotor.class, "leftRearLift");
+        rfl = hardwareMap.get(DcMotor.class, "RightFrontLift");
+        rrl = hardwareMap.get(DcMotor.class, "RightRearLift");
+        */
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         // Wait for the game to start (driver presses PLAY)
@@ -28,21 +41,27 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             double leftX = gamepad1.left_stick_x;
-            double leftY = gamepad1.left_stick_y;
+            double leftY = gamepad1.left_stick_y * -1;
+            double rightY = gamepad1.right_stick_y * -1;
             telemetry.addData("leftX", "" + leftX);
             telemetry.addData("leftY", "" + leftY);
+            telemetry.addData("rightY", "" + rightY);
             if (leftX == 0.0 && leftY == 0.0) {
-                // rotation stuff
+                double rDepth = gamepad1.right_trigger;
+                double lDepth = gamepad1.left_trigger;
+                rotate(lDepth, rDepth);
             } else {
                 translate(leftX, leftY);
             }
+            //lift(rightY);
+            telemetry.update();
         }
+
     }
-    private double pythag(double num1, double num2) {
+    public double pythag(double num1, double num2) {
         return Math.sqrt(Math.pow(num1, 2) + Math.pow(num2, 2));
     }
-    // sets 4 drive motors for translational movement
-    private void translate(double xVal, double yVal) {
+    public void translate(double xVal, double yVal) {
         double totalPower = pythag(xVal, yVal);
         if (totalPower > 1) totalPower = 1;
         if (totalPower < -1) totalPower = -1;
@@ -77,6 +96,30 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             rfd.setPower(rMod * oppPower);
         }
     }
-
+    public void lift(double power) {
+        lfl.setPower(power);
+        lrl.setPower(power);
+        rfl.setPower(power);
+        rrl.setPower(power);
+        // TODO: multiply powers by -1 when needed
+    }
+    public void rotate(double lDepth, double rDepth) {
+        if (rDepth > 0) {
+            lfd.setPower(rDepth);
+            lrd.setPower(rDepth);
+            rfd.setPower(-1*rDepth);
+            rrd.setPower(-1*rDepth);
+        } else if (lDepth > 0){
+            lfd.setPower(-1*lDepth);
+            lrd.setPower(-1*lDepth);
+            rfd.setPower(lDepth);
+            rrd.setPower(lDepth);
+        } else {
+            lfd.setPower(0.0);
+            lrd.setPower(0.0);
+            rfd.setPower(0.0);
+            rrd.setPower(0.0);
+        }
+    }
 }
 
