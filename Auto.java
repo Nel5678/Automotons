02e722664b6b6583a21bfabc03c76a39b3c47f49
DelphinusArcana <146129635+DelphinusArcana@users.noreply.github.com>
2024-01-24 16:39:57 -0500

    Update Auto.java
    Ran this on the robot and it works so yay we have autonomous

--- Auto.java
+++ Auto.java
@@ -1,7 +1,6 @@
 
 package org.firstinspires.ftc.Automotons;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
@@ -10,20 +9,22 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name="MyAutonomousOpMode")
-public class MyAutonomousOpMode extends Autonomous{
+public class Auto extends LinearOpMode{
     private DcMotor lfd;
     private DcMotor lrd;
     private DcMotor rfd;
     private DcMotor rrd;
-    @Override public void main() throws InterruptedException
-    {
-        lfd = hardwareMap.get(DcMotor.class, "leftFrontDrive");
-        lrd = hardwareMap.get(DcMotor.class, "leftRearDrive");
-        rfd = hardwareMap.get(DcMotor.class, "rightFrontDrive");
-        rrd = hardwareMap.get(DcMotor.class, "rightRearDrive");
-        lrd.setTargetPosition(lrd.getCurrentPosition() + 1000);
-        while (lrd.getCurrentPosition()) {
-
+    @Override
+    public void runOpMode () {
+        telemetry.addData("Status", "running Auto");
+        telemetry.update();
+        waitForStart();
+        int counter = 0;
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Auto after start");
+            telemetry.addData("Counter", counter);
+            counter++;
+            telemetry.update();
         }
     }
 }
