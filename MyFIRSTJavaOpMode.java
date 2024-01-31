c2fd2bfa547cf798ecb9ff12f2f71877d3f14d25
DelphinusArcana <146129635+DelphinusArcana@users.noreply.github.com>
2024-01-31 15:26:05 -0500

    Update MyFIRSTJavaOpMode.java
    Working on lift stuff now

--- MyFIRSTJavaOpMode.java
+++ MyFIRSTJavaOpMode.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.Automotons;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -8,9 +7,13 @@ import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Template: 2024 Brie 2", group="Linear Opmode")
+@TeleOp(name="Template: 2024 Brie 3", group="Linear Opmode")
 public class MyFIRSTJavaOpMode extends LinearOpMode {
+    // motor directions
+    private DcMotorSimple.Direction forward = DcMotor.Direction.FORWARD;
+    private DcMotorSimple.Direction reverse = DcMotor.Direction.REVERSE;
     // drive stuff
     private DcMotor lfd;
     private DcMotor lrd;
@@ -32,6 +35,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     private double maxChangePower = 0.2;
     private int goodEnoughDistance = 100;
     private int maxPowerDistance = 500;
+    private int liftTarget = 0;
     // plane stuff
     private Servo planeServo;
     private double planeStart = 0.5;
@@ -39,10 +43,10 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     // claw stuff
     private Servo lClaw;
     private Servo rClaw;
-    private double leftOpenPos = /*(double) 1/2*/ 0.703125;
-    private double leftDiff = (double) -3/8;
-    private double rightOpenPos = /*(double) 37/128*/ 0.296875;
-    private double rightDiff = (double) 3/8;
+    private static double leftOpenPos = 0.703125;
+    private static double leftDiff = (double) -3/8;
+    private static double rightOpenPos = 0.296875;
+    private static double rightDiff = (double) 3/8;
     // drum stuff
 
     @Override
@@ -51,16 +55,21 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         lrd = hardwareMap.get(DcMotor.class, "leftRearDrive");
         rfd = hardwareMap.get(DcMotor.class, "rightFrontDrive");
         rrd = hardwareMap.get(DcMotor.class, "rightRearDrive");
-        lfd.setDirection(DcMotorSimple.Direction.FORWARD);
-        lrd.setDirection(DcMotorSimple.Direction.REVERSE);
-        rfd.setDirection(DcMotorSimple.Direction.REVERSE);
-        rrd.setDirection(DcMotorSimple.Direction.FORWARD);
 
-/*
+        lfd.setDirection(forward);
+        lrd.setDirection(forward);
+        rfd.setDirection(reverse);
+        rrd.setDirection(reverse);
+
         lfl = hardwareMap.get(DcMotor.class, "leftFrontLift");
         lrl = hardwareMap.get(DcMotor.class, "leftRearLift");
         rfl = hardwareMap.get(DcMotor.class, "rightFrontLift");
         rrl = hardwareMap.get(DcMotor.class, "rightRearLift");
+
+        lfl.setDirection(reverse);
+        lrl.setDirection(forward);
+        rfl.setDirection(reverse);
+        rrl.setDirection(reverse);
 
         lflStart = lfl.getCurrentPosition();
         lrlStart = lrl.getCurrentPosition();
@@ -71,7 +80,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         lrlDistance = 0;
         rflDistance = 0;
         rrlDistance = 0;
-
+/*
         planeServo = hardwareMap.get(Servo.class, "planeServo");
         planeServo.setPosition(planeStart);
 
@@ -83,8 +92,13 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         boolean rightPressed = false;
         boolean leftClosed = false;
         boolean rightClosed = false;
+
+        ElapsedTime runtime = new ElapsedTime();
+        runtime.reset();
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
@@ -93,11 +107,11 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             telemetry.addData("leftRearPosition", lrd.getCurrentPosition());
             double leftX = gamepad1.left_stick_x * 0.1;
             double leftY = gamepad1.left_stick_y * -0.1;
-            double rightY = gamepad1.right_stick_y * -1;
+            double rightY = gamepad1.right_stick_y * -0.3;
             telemetry.addData("leftX", "" + leftX);
             telemetry.addData("leftY", "" + leftY);
             telemetry.addData("rightY", "" + rightY);
-            if (leftX == 0.0 && leftY == 0.0) {
+            /*if (leftX == 0.0 && leftY == 0.0) {
                 double rDepth = gamepad1.right_trigger;
                 double lDepth = gamepad1.left_trigger;
                 rotate(lDepth, rDepth);
@@ -105,11 +119,8 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             } else {
                 translate(leftX, leftY);
                 telemetry.addData("", "translateCalled");
-            }
-            telemetry.addData("lfdPower", lfd.getPower());
-            telemetry.addData("lrdPower", lrd.getPower());
-            telemetry.addData("rfdPower", rfd.getPower());
-            telemetry.addData("rrdPower", rrd.getPower());
+            }*/
+            //addDriveTelemetry();
             /*if (gamepad1.left_bumper && !leftPressed) {
                 leftClosed = !leftClosed;
                 leftPressed = true;
@@ -124,16 +135,28 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
                 rightPressed = false;
             }
             setClaw(false, rightClosed);*/
-            //lift(rightY);
-            //updateLiftDistances();
+            lift(rightY);
+            addLiftTelemetry();
             telemetry.update();
         }
 
     }
-    public double pythag(double num1, double num2) {
+    public void addDriveTelemetry () {
+        telemetry.addData("lfd power", lfd.getPower() + ", pos: " + lfd.getCurrentPosition());
+        telemetry.addData("lrd power", lrd.getPower() + ", pos: " + lrd.getCurrentPosition());
+        telemetry.addData("rfd power", lfd.getPower() + ", pos: " + rfd.getCurrentPosition());
+        telemetry.addData("rrd power", rrd.getPower() + ", pos: " + rrd.getCurrentPosition());
+    }
+    public void addLiftTelemetry () {
+        telemetry.addData("lfl power", lfl.getPower() + ", dist: " + lflDistance);
+        telemetry.addData("lrl power", lrl.getPower() + ", dist: " + lrlDistance);
+        telemetry.addData("rfl power", rfl.getPower() + ", dist: " + rflDistance);
+        telemetry.addData("rrl power", rrl.getPower() + ", dist: " + rrlDistance);
+    }
+    public static double pythag(double num1, double num2) {
         return Math.sqrt(Math.pow(num1, 2) + Math.pow(num2, 2));
     }
-    public void translate(double xVal, double yVal) {
+    public static void translate(double xVal, double yVal, DcMotor lfd, DcMotor lrd, DcMotor rfd, DcMotor rrd) {
         double totalPower = pythag(xVal, yVal);
         if (totalPower > 1) totalPower = 1;
         if (totalPower < -1) totalPower = -1;
@@ -168,16 +191,8 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             rfd.setPower(oppPower);
         }
     }
-    public void lift(double power) {
-        double lflMod = changeFromDistance(lflDistance);
-        double lrlMod = changeFromDistance(lrlDistance);
-        double rflMod = changeFromDistance(rflDistance);
-        double rrlMod = changeFromDistance(rrlDistance);
-
-        lfl.setPower((-1 * power) + lflMod);
-        lrl.setPower((-1 * power) + lrlMod);
-        rfl.setPower(power + rflMod);
-        rrl.setPower(power + rrlMod);
+    public void translate(double xVal, double yVal) {
+        translate(xVal, yVal, lfd, lrd, rfd, rrd);
     }
     public void rotate(double lDepth, double rDepth) {
         if (rDepth > 0) {
@@ -197,8 +212,20 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             rrd.setPower(0.0);
         }
     }
+    public void lift(double power) {
+        updateLiftDistances();
+        double lflMod = changeFromDistance(lflDistance);
+        double lrlMod = changeFromDistance(lrlDistance);
+        double rflMod = changeFromDistance(rflDistance);
+        double rrlMod = changeFromDistance(rrlDistance);
+
+        lfl.setPower(power + lflMod);
+        lrl.setPower(power + lrlMod);
+        rfl.setPower(power + rflMod);
+        rrl.setPower(power + rrlMod);
+    }
     public void updateLiftDistances() {
-        lflDistance = lfl.getCurrentPosition() - lflStart;
+        lflDistance = (lfl.getCurrentPosition() - lflStart) * -1;
         lrlDistance = lrl.getCurrentPosition() - lrlStart;
         rflDistance = rfl.getCurrentPosition() - rflStart;
         rrlDistance = rrl.getCurrentPosition() - rrlStart;
@@ -207,9 +234,10 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         return (lflDistance + lrlDistance + rflDistance + rrlDistance)/4;
     }
     public double changeFromDistance(double distance) {
+        distance -= averageLiftDistance();
         if (distance > maxPowerDistance)
             return maxChangePower;
-        if (distance < (-1.0 * maxChangePower))
+        if (distance < (-1.0 * maxPowerDistance))
             return -1.0 * maxChangePower;
         if (Math.abs(distance) < goodEnoughDistance)
             return 0.0;
@@ -218,21 +246,26 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     public void launchPlane () {
         planeServo.setPosition(planeStart + planeDiff);
     }
-    private void setClaw (boolean settingLeft, boolean closed) {
+    public static void setClaw (boolean settingLeft, boolean closed, Servo claw) {
         if (settingLeft) {
             if (closed) {
-                lClaw.setPosition(leftOpenPos + leftDiff);
-
+                claw.setPosition(leftOpenPos + leftDiff);
             } else {
-                lClaw.setPosition(leftOpenPos);
+                claw.setPosition(leftOpenPos);
             }
         } else {
             if (closed) {
-                rClaw.setPosition(rightOpenPos + rightDiff);
+                claw.setPosition(rightOpenPos + rightDiff);
             } else {
-                rClaw.setPosition(rightOpenPos);
+                claw.setPosition(rightOpenPos);
             }
         }
+    }
+    public void setClaw (boolean settingLeft, boolean closed) {
+        if (settingLeft)
+            setClaw(true, closed, lClaw);
+        else
+            setClaw(false, closed, rClaw);
     }
 }
 
